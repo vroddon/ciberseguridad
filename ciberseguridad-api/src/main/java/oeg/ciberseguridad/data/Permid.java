2147029510fd81a51f88a501a0bd50341b771e53
Víctor Rodríguez Doncel <victorr@ac.upc.edu>
2019-06-17 18:55:37 +0200

    asdf

--- ciberseguridad-api/src/main/java/oeg/ciberseguridad/data/Permid.java
+++ ciberseguridad-api/src/main/java/oeg/ciberseguridad/data/Permid.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import static oeg.ciberseguridad.data.CompaniesHouse.sortByValue;
 
 /**
  *
@@ -37,24 +38,60 @@ public class Permid {
             }
             List<String> hits = new ArrayList();
             count=0;
+            Map<String, Integer> mapa = new HashMap();
             try (BufferedReader br = new BufferedReader(new FileReader(file))) {
                 String line;
                 while ((line = br.readLine()) != null) {
                     if (line.contains("<http://ont.thomsonreuters.com/mdaas/HeadquartersAddress>"))
                     {
-                    //    System.out.print(count+"\r");
                         for(int k =0;k<empresas.size();k++)
                         {
                             String empresa = empresas.get(k);
                             if (line.contains(empresa))
                             {
-                                hits.add(line);
-                                System.out.println(line);
+                                try{
+                  //              System.out.println(line);
+                                    int index = line.indexOf(" ");
+                                    int index2 = line.indexOf(" ", index+1);
+                                    String pais = line.substring(index2,line.length());
+                                    int index3 =  pais.lastIndexOf("\\n");
+                                    int index4 = pais.lastIndexOf("\\n", index3-1);
+                                    pais = pais.substring(index4+2, index3);
+                                    pais = pais.replace("\"", "");
+                                    Integer total = mapa.get(pais);
+                                    if (total==null)
+                                        total =0;
+                                    total++;
+                                    mapa.put(pais,total);
+                                    hits.add(pais);
+                                    count++;
+                                    System.out.println(count+"\t"+pais);
+                                    if (count>=1500)
+                                        break;
+                                    
+                                }catch(Exception ex3)
+                                {
+                                    
+                                }
+
                             }
+                            if (count>=1500)
+                                break;                            
                         }
                     }
-                    count++;
+                    
                 }
+                
+            mapa = sortByValue(mapa);
+            ArrayList keyList = new ArrayList(mapa.keySet());
+            for (int i = keyList.size() - 1; i >= 0; i--) {
+                String key = (String) keyList.get(i);
+                Integer value = mapa.get(key);
+                System.out.println(key + "\t" + value);
+            }
+                
+                
+                
             } catch (Exception e) {
             }            
             for(String hit : hits)
